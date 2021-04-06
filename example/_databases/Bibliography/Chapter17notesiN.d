54b8f6334af97be0f4f1776be7ddd9a3e1ef16b5
fornib <b@forni.me>
2021-04-06 13:49:41 +0200

    update data

--- example/_databases/Bibliography/Chapter17notesiN.d
+++ example/_databases/Bibliography/Chapter17notesiN.d
@@ -1,5 +1,4 @@
 @NOTES R/17.1
-
 NA iN
 ND December 2020
 TO spiritual metaphor; historicity
@@ -7,7 +6,6 @@ TO spiritual metaphor; historicity
 This book dissects the concept of historicity within biblical traditions, arguing that the inherent purpose of utilizing historical events within scripture is to illustrate spiritual metaphors. See {B}R/Moore2009symbol
 
 @NOTES R/17.2
-
 NA iN
 ND December 2020
 TO rhetoric; argumentation
@@ -15,7 +13,6 @@ TO rhetoric; argumentation
 This article disagrees with this assessment and demonstrates deductive reasoning through rhetorical questions present in portions of Genesis and Kings. See {B}R/Moshavi2015rhetoric
 
 @NOTES R/17.4
-
 NA iN
 ND December 2020
 TO differentiation; myth patterns
@@ -23,7 +20,6 @@ TO differentiation; myth patterns
 For a description of "myth patterns" in creation traditions of the Ancient Near East and in the Hebrew Bible, see {B}R/Toombs1961mythpatterns
 
 @NOTES R/17.4
-
 NA iN
 ND December 2020
 TO Narratology; function
@@ -32,7 +28,6 @@ For an analysis on the societal function of myths and how their narrative forms 
 
 
 @NOTES R/17.6
-
 NA iN
 ND December 2020
 TO messianic themes; cultural milieu 
@@ -40,15 +35,11 @@ TO messianic themes; cultural milieu
 Messianic themes existed in ancient Egyptian Middle Kingdom texts such as the **Prophecies of Neferty, The Lamentations of Khakheperre-Sonbe, and the Admonitions of Ipuwer**. These texts narrativize the chaotic state of society utilizing literary tropes to juxtapose the promise of deliverance from future leaders. 
 
 @NOTES R/17.7
-
 NA iN
 ND December 2020
 TO nature of myths; public consumption
 
 For an overview of how the common people of Mesopotamia may have digested mythic traditions and practiced religion, see {B}R/Nefat2007Commonpeople
-
-
-
 
 
 
---
+++ example/_website/Akk-lg/Akk-lgNotes-1.d
@@ -0,0 +1,1512 @@
+
+@NOTES Lg/1
+
+See {B}Lg/MatousPetracek1956Liquiden.
+
+@NOTES Lg/1
+
+See Reiner 1964 Subsystem.
+
+@NOTES Lg/1
+
+See Reiner 1964 Analysis.
+
+@NOTES Lg/1
+
+See Von Soden and R&ouml;llig 1967 Syllabar.
+
+
+
+
+@NOTES Lg/1.1
+
+See {B}Lg/Goetze1937Nazarum.
+
+@NOTES Lg/1.1
+
+See {B}Lg/Speiser1947Summa.
+
+@NOTES Lg/1.1
+
+See {B}Lg/Cantineau1951-52Consonantisme. 
+
+@NOTES Lg/1.1
+
+See {B}Lg/Martinet1953Consonantisme.
+
+@NOTES Lg/1.1
+
+See Speiser 1953 Comments.
+
+@NOTES Lg/1.1
+
+See {B}Lg/Moscati1954SistemaConsonantico.
+
+@NOTES Lg/1.1
+
+See Rundgren 1955 *Bildungen* 140-45.
+
+@NOTES Lg/1.1
+
+See {B}Lg/Goetze1958Sibilants.
+
+
+
+
+@NOTES Lg/1.2
+
+See {B}Lg/Goetze1947ShortA.
+
+
+
+
+@NOTES Lg/1.3
+
+See Sarauw 1939 *Akzent*.
+
+@NOTES Lg/1.2
+
+See {B}Lg/Fleisch1947-43Systeme.
+
+@NOTES Lg/1.2
+
+See {B}Lg/VonSoden1952UnregelmassigeVerben.
+
+@NOTES Lg/1.3
+
+See Kurylowicz 1951 *Apophonie* 52-53.
+
+@NOTES Lg/1.3
+
+See Reiner 1966 *Analysis* 38-39.
+
+@NOTES Lg/1.3
+
+See {B}Lg/Hetzron1969Y'aqtul-Yaqt'ul.
+
+
+
+
+@NOTES Lg/1.4
+
+See {B}Lg/VonSoden1948Vokalfarbungen.
+
+@NOTES Lg/1.4
+
+See {B}Lg/Knudsen1961QPhoneme.
+
+@NOTES Lg/1.4
+
+See {B}Lg/VonSoden1968Spirantisierung.
+
+@NOTES Lg/1.4
+
+See {B}Lg/Knudsen1969Sprantization.
+
+
+
+
+@NOTES Lg/2
+
+See Goetze 1946 "Syllables".
+
+@NOTES Lg/2
+
+See Goetze 1947 "Short a?"
+
+@NOTES Lg/2
+
+See {B}Lg/Cantineau1949VoyelledeSecours.
+
+@NOTES Lg/2
+
+See {B}Lg/MatousPetracek1956Liquiden.
+
+@NOTES Lg/2
+
+See Reiner 1966 *Analysis* 42-53.
+
+
+
+
+@NOTES Lg/4
+
+See Reiner 1966 *Analysis* 42-53.
+
+
+
+
+@NOTES Lg/5
+
+See {B}Lg/Blake1945StudiesIII. 
+
+@NOTES Lg/5
+
+See {B}Lg/Labaet1946SystemePhonetique.
+
+@NOTES Lg/5
+
+See Speiser 1953 "Comments".
+
+@NOTES Lg/5
+
+See {B}Lg/Goetze1958Sibilants.
+
+
+
+
+
+@NOTES Lg/6
+
+See {B}Lg/Fischer1962-64Tripartite.
+
+
+
+
+@NOTES Lg/6.1
+
+See {B}Lg/Fronzaroli1966Tipologia.
+
+
+
+
+@NOTES Lg/6.2
+
+See THUREAU-DANGlN, "La correspoadunce de Hammurapi avec S^ame&#253;-hasir," *RA* 21(1924). p.7,n,2.
+
+@NOTES Lg/6.2
+
+See {B}Lg/Fleisch1956Esquissa.
+
+@NOTES Lg/6.2
+
+See {B}Lg/Fleisch1959AspectLexical.
+
+@NOTES Lg/6.2
+
+See {B}Lg/VonSoden1960StatusRectus-Formen.
+
+@NOTES Lg/6.2
+
+See Kurylowicz 1961 *Apophonie* p. 156-57.
+
+@NOTES Lg/6.2
+
+See {B}Lg/Edzard1962NigalGamil.
+
+@NOTES Lg/6.2
+
+See {B}Lg/Eilers1964Nominalformen.
+
+@NOTES Lg/6.2
+
+See Reiner 1966 *Analysis* 105-6.
+
+
+
+
+@NOTES Lg/6.3
+
+See {B}Lg/Botterweck1952Triliterismus.
+
+@NOTES Lg/6.3
+
+See Von Soden 1952 *Grundriss* par. 55 and 57 a-b.
+
+@NOTES Lg/6.3
+
+See {B}Lg/Greenberg1955Internal-a-Plural.
+
+@NOTES Lg/6.3
+
+See {B}Lg/Kienast1961R-Stamm.
+
+@NOTES Lg/6.3
+
+See {B}Lg/Kienast1957Reduplikation.
+
+@NOTES Lg/6.3
+
+See {B}Lg/Petracek1960-62InnereFlexion.
+
+@NOTES Lg/6.3
+
+See Castellino 1962 *System* 122.
+
+
+
+
+
+@NOTES Lg/6.4
+
+See {B}Lg/Fronzaroli1966Tipologia.
+
+@NOTES Lg/6.4
+
+See Gelb 1969 *Sequential* 133-35.
+
+
+
+
+
+@NOTES Lg/7
+
+See {B}Lg/Cantineau1950NotiondeScheme.
+
+@NOTES Lg/7
+
+See {B}Lg/Cantineau1950RacinesetSchemes.
+
+@NOTES Lg/7
+
+See {B}Lg/Fleisch1956Esquissa.
+
+@NOTES Lg/7
+
+See {B}Lg/Fleisch1961Traite.
+
+@NOTES Lg/7
+
+See {B}Lg/Petracek1960-62InnereFlexion.
+
+@NOTES Lg/7
+
+See {B}Lg/Cohen1965DerivationNominale.
+
+
+
+
+
+@NOTES Lg/7
+
+See Reiner 1966 *Analysis*, at least for the noun.
+
+@NOTES Lg/7
+
+See Gelb 1969 *Sequential* 161-65.
+
+
+
+
+
+@NOTES Lg/8
+
+See {B}Lg/Goetze1945Heidel.
+
+@NOTES Lg/8
+
+See {B}Lg/Cantineau1950NotiondeScheme.
+
+@NOTES Lg/8
+
+See {B}Lg/Cantineau1950RacinesetSchemes.
+
+@NOTES Lg/8
+
+See Fleisch 1956 *Structure* 22 ff.
+
+@NOTES Lg/8
+
+See Gelb 1969 *Sequential* 163-65.
+
+
+
+
+
+@NOTES Lg/9
+
+See {B}Lg/Landsberger1938Gestalt.
+
+@NOTES Lg/9
+
+See {B}Lg/Geers1965Emphatics.
+
+@NOTES Lg/9
+
+See {B}Lg/Greenberg1950Patterning.
+
+@NOTES Lg/9
+
+See {B}Lg/Knudsen1961QPhoneme.
+
+@NOTES Lg/9
+
+See {B}Lg/Petracek1964Inkompatibilitat.
+
+@NOTES Lg/9
+
+See Reiner 1966 *Analysis* 41, 49-51.
+
+
+
+
+
+@NOTES Lg/10
+
+See {B}Lg/Bauer1914Sprachtypus.
+
+@NOTES Lg/10
+
+See {B}Lg/Landsberger1927Eigenbegrifflichkeit.
+
+@NOTES Lg/10
+
+See {B}Lg/Landsberger1938Gestalt.
+
+@NOTES Lg/10
+
+See {B}Lg/Greenberg1950Patterning.
+
+@NOTES Lg/10
+
+See {B}Lg/Knudsen1961QPhoneme.
+
+@NOTES Lg/10
+
+See {B}Lg/Fronzaroli1963ElementoVocalico.
+
+@NOTES Lg/10
+
+See Gelb 1969 *Sequential* 160-65.
+
+
+
+
+
+@NOTES Lg/11
+
+See {B}Lg/Barth1894Nominalbildung.
+
+@NOTES Lg/11
+
+See {B}Lg/Nyberg1920Wortbildung.
+
+@NOTES Lg/11
+
+See {B}Lg/Eilers1936Nominalbildung.
+
+@NOTES Lg/11
+
+See {B}Lg/Fleisch1956Esquissa.
+
+@NOTES Lg/11
+
+See Kurylowicz 1961 *Apophonie* esp.pp. 39-41; 101 ff.; 140 ff.; par. 157.
+
+@NOTES Lg/11
+
+See {B}Lg/Eilers1964Nominalformen.
+
+
+
+
+
+@NOTES Lg/11
+
+See {B}Lg/Peters1942Qutulla about specific patterns
+
+@NOTES Lg/11
+
+See {B}Lg/Speiser1952Elative about specific patterns
+
+@NOTES Lg/11
+
+See {B}Lg/Moscati1962ParticipioPassivo about specific patterns
+
+
+
+
+
+@NOTES Lg/11
+
+See {B}Lg/Cohen1965DerivationNominale on extended patterns as morphophonemic
+
+
+
+
+
+@NOTES Lg/13
+
+See ch. 17.
+
+
+
+
+
+@NOTES Lg/14
+
+See {B}Lg/Goetze1947Passive.
+
+@NOTES Lg/14
+
+See Aro 1964 *Vokalisierung* for the vocalism of the root vowel.
+
+
+
+
+
+@NOTES Lg/14
+
+See Kurylowicz 1961 *Apophonie*, 30.
+
+
+
+
+
+@NOTES Lg/14
+
+See {B}Lg/Landsberger1938Gestalt.
+
+@NOTES Lg/14
+
+See Kurylowicz 1961 *Apophonie*, p.17.
+
+@NOTES Lg/14
+
+See {B}Lg/Fronzaroli1963ElementoVocalico.
+
+
+
+
+
+@NOTES Lg/15
+
+See {B}Lg/Landsberger1927Eigenbegrifflichkeit.
+
+@NOTES Lg/15
+
+See {B}Lg/Cohen1955VerbesDeponents.
+
+@NOTES Lg/15
+
+See R&#363;ssler 1951 "Verbum".
+
+@NOTES Lg/15
+
+See Castellino 1962 *System* esp. pp. 10,15,45-49.
+
+@NOTES Lg/15
+
+See Aro 1964 *Vokalisierung*.
+
+@NOTES Lg/15
+
+See {B}Lg/Kienast1967Vokalklassen.
+
+
+
+
+
+@NOTES Lg/17
+
+See {B}Lg/Bauer1910Tempora.
+
+@NOTES Lg/17
+
+See {B}Lg/Christian1927Tempora.
+
+@NOTES Lg/17
+
+See Lander 1928 "Tempora".
+
+@NOTES Lg/17
+
+See {B}Lg/Blake1942StudiesII.
+
+@NOTES Lg/17
+
+See {B}Lg/Goetze1942Intensive.
+
+@NOTES Lg/17
+
+See Fleisch 1942-48 "Syst&#232;me".
+
+@NOTES Lg/17
+
+See R&#363;ssler 1951 "Verbum".
+
+@NOTES Lg/17
+
+See R&#363;ssler 1952 "Charakter".
+
+@NOTES Lg/17
+
+See Castellino 1962 *System*.
+
+@NOTES Lg/17
+
+See {B}Lg/Edzard1965Stemme.
+
+
+
+
+
+@NOTES Lg/17.1
+
+See {B}Lg/Greenberg1952Present.
+
+@NOTES Lg/17.1
+
+See {B}Lg/Bloch1963Iparras.
+
+
+
+
+
+@NOTES Lg/17.2
+
+See {B}Lg/Hetzron1969Y'aqtul-Yaqt'ul.
+
+
+
+
+
+@NOTES Lg/17.3
+
+See Gelb 1969 *Sequential* 172-74.
+
+
+
+
+
+@NOTES Lg/18
+
+See {B}Lg/Hurwitz1913RootDeterminatives.
+
+@NOTES Lg/18
+
+See Heidel 1940 *Quadriliteral*.
+
+@NOTES Lg/18
+
+See {B}Lg/Goetze1945Heidel.
+
+@NOTES Lg/18
+
+See {B}Lg/Goetze1947Passive.
+
+@NOTES Lg/18
+
+See {B}Lg/VonSoden1950Heidel.
+
+@NOTES Lg/18
+
+See Von Soden 1951 "N-St&#228;mme".
+
+@NOTES Lg/18
+
+See {B}Lg/Fleisch1956Esquissa.
+
+
+
+
+
+@NOTES Lg/20
+
+See {B}Lg/Buccellati1968Stative,
+
+@NOTES Lg/20
+
+See Gelb 1969 *Sequential*.
+
+@NOTES Lg/20
+
+See {B}Lg/Landsberger1926Prinzipienfragen.
+
+
+
+
+
+@NOTES Lg/21
+
+See {B}Lg/VonSoden1939Adverbialisendung.
+
+@NOTES Lg/21
+
+See {B}Lg/Oppenheim1947Deictic.
+
+@NOTES Lg/21
+
+See {B}Lg/Eilers1964Nominalformen.
+
+@NOTES Lg/21
+
+See {B}Lg/Cohen1965DerivationNominale.
+
+@NOTES Lg/21
+
+See Gelb 1967 "Morpheme an"
+
+@NOTES Lg/21
+
+See Gelb 1968 "Dragoman"
+
+@NOTES Lg/21
+
+See Buccellati 1966 *The Amorites of the Ur III Period,* Naples, pp. 224-229;.
+
+@NOTES Lg/21
+
+See Gelb 1969 *Sequential* 166-170.
+
+
+
+
+
+
+@NOTES Lg/22
+
+See Gelb 1969 *Sequential*.
+
+
+
+
+
+@NOTES Lg/22.1
+
+See {B}Lg/Christian1924Kasusendungen.
+
+@NOTES Lg/22.1
+
+See {B}Lg/Goetze1946Plural.
+
+@NOTES Lg/22.1
+
+See {B}Lg/Bravmann1947PluralEnding.
+
+@NOTES Lg/22.1
+
+See {B}Lg/Fleisch1961Traite.
+
+@NOTES Lg/22.1
+
+See Kurylowicz 1961 *Apophonie*, 122-24.
+
+
+
+
+
+@NOTES Lg/22.2
+
+See {B}Lg/Brockelmann1903Femininendung.
+
+@NOTES Lg/22.2
+
+See F&#233;chali and Cuny 1924 "Genre grammatical".
+
+@NOTES Lg/22.2
+
+See Schade 1927 "Geschlecht"
+
+@NOTES Lg/22.2
+
+See Wensinck 1927 "Gender"
+
+@NOTES Lg/22.2
+
+See Speiser 1936 "Studies"
+
+@NOTES Lg/22.2
+
+See Jakobsen 1960 "Ittalak niati" 114 f.
+
+@NOTES Lg/22.2
+
+See Kurylowicz 1961 *Apophonie*, pp. 36, 148-50.
+
+
+
+
+
+@NOTES Lg/22.3
+
+See Barth 1982 "Flexion".
+
+@NOTES Lg/22.3
+
+See {B}Lg/Sarauw1905-6Kasussystem.
+
+@NOTES Lg/22.3
+
+See {B}Lg/Nyberg1920Wortbildung.
+
+@NOTES Lg/22.3
+
+See Bravmann 1961 "Genitive".
+
+@NOTES Lg/22.3
+
+See Gelb 1969 *Sequential* 180.
+
+
+
+
+
+@NOTES Lg/22.4
+
+See Tagliavini 1929 "Mimazione".
+
+@NOTES Lg/22.4
+
+See Gelb 1930 "Mimazione".
+
+@NOTES Lg/22.4
+
+See {B}Lg/Kurylowicz1950Mimation.
+
+@NOTES Lg/22.4
+
+See Jirku 1953 "Mimation".
+
+@NOTES Lg/22.4
+
+See {B}Lg/VonSoden1960StatusRectus-Formen.
+
+@NOTES Lg/22.4
+
+See Kurylowicz 1961 *Apophonie* pp. 160-62.
+
+@NOTES Lg/22.4
+
+See Gelb 1969 *Sequential* esp. 15-16, 138-46.
+
+
+
+
+
+@NOTES Lg/22.5
+
+See {B}Lg/Barth1914AssyrischesIs.
+
+@NOTES Lg/22.5
+
+See Von Soden 1932 "Hymnisch-epische" 90-130.
+
+@NOTES Lg/22.5
+
+See Sarauw 1939 *Akzent* 46.
+
+@NOTES Lg/22.5
+
+See Speiser 1953 "Comments".
+
+@NOTES Lg/22.5
+
+See {B}Lg/Speiser1953Terminative.
+
+@NOTES Lg/22.5
+
+See Rundgren 1955 *Bildungen*, pp. 38, 157-69.
+
+@NOTES Lg/22.5
+
+See {B}Lg/Kienast1957Erwagungen.
+
+@NOTES Lg/22.5
+
+See {B}Lg/VonSoden1960StatusRectus-Formen.
+
+
+
+
+
+@NOTES Lg/23
+
+See Von Soden 1931 "Hymnisch-episch Dialekt" 179; 208-227.
+
+@NOTES Lg/23
+
+See {B}Lg/Nougayrol1950Determination.
+
+@NOTES Lg/23
+
+See {B}Lg/MatousPetracek1956Liquiden.
+
+@NOTES Lg/23
+
+See Kurylowicz 1961 *Apophonie* par. 229-33.
+
+@NOTES Lg/23
+
+See Rosen 1962 *Textbook* 89;292.
+
+
+
+
+
+@NOTES Lg/23.1
+
+See Von Soden 1954 "Eine altbabylonische Beschworung gegen die Damonin lama&#353;tum," *Or.*NS 23, 343f.
+
+@NOTES Lg/23.1
+
+See {B}Lg/VonSoden1960StatusRectus-Formen.
+
+@NOTES Lg/23.1
+
+See Reiner 1966 *Analysis* 125-27.
+
+
+
+
+
+@NOTES Lg/24
+
+See Gelb 1950 "S^ullat and Hani&#353;," *Ar.Or.* 18, 189.
+
+@NOTES Lg/24
+
+See {B}Lg/VonSoden1960StatusRectus-Formen.
+
+@NOTES Lg/24
+
+See {B}Lg/VonSoden1961Zahlen.
+
+
+
+
+
+@NOTES Lg/25
+
+See {B}Lg/Buccellati1968Stative.
+
+@NOTES Lg/25
+
+See {B}Lg/Goetze1942Intensive.
+
+@NOTES Lg/25
+
+See Aro 1965 "Stative".
+
+
+
+
+
+@NOTES Lg/27
+
+See Von Soden 1932 "Hymnisch-epische Dialekt" 148-51.
+
+@NOTES Lg/27
+
+See {B}Lg/Jacobsen1960IttallakNiati.
+
+@NOTES Lg/27
+
+See Gelb 1969 *Sequential* esp. 187-202.
+
+
+
+
+@NOTES Lg/28
+
+See Kienast 1960 "japrus" 152-53.
+
+@NOTES Lg/28
+
+See {B}Lg/Jacobsen1960IttallakNiati.
+
+@NOTES Lg/28
+
+See Kurylowicz 1960 *Apophonie* 52-55.
+
+@NOTES Lg/28
+
+See {B}Lg/VonSoden1965Perfekt.
+
+@NOTES Lg/28
+
+See {B}Lg/Fleisch1966PhraseRelative.
+
+@NOTES Lg/28
+
+See {B}Lg/Fleisch1968Yaqtula.
+
+@NOTES Lg/28
+
+See Gelb 1969 *Sequential* 96-107.
+
+
+
+
+@NOTES Lg/28.1
+
+See {B}Lg/Hetzron1969Y'aqtul-Yaqt'ul.
+
+
+
+
+
+
+@NOTES Lg/28.2
+
+See {B}Lg/Landsberger1924Ventiv.
+
+@NOTES Lg/28.2
+
+See {B}Lg/Fleisch1947-43Systeme.
+
+@NOTES Lg/28.2
+
+See {B}Lg/Jacobsen1960IttallakNiati.
+
+@NOTES Lg/28.2
+
+See {B}Lg/VonSoden1961Akkusativ.
+
+@NOTES Lg/28.2
+
+See {B}Lg/Jacobsen1963AblativeAccusative.
+
+@NOTES Lg/28.2
+
+See Gelb 1969 *Sequential* esp. 105; 136-37.
+
+
+
+
+
+
+@NOTES Lg/30
+
+See {B}Lg/Greenberg1960Agreement.
+
+@NOTES Lg/30
+
+See {B}Lg/VonSoden1961Akkadisch.
+
+@NOTES Lg/30
+
+See Reiner 1966 *Analysis* 98-102.
+
+@NOTES Lg/30
+
+See Gelb 1969 *Sequential* 38, 145 f., 181.
+
+@NOTES Lg/30
+
+See {B}Lg/Pennacchietti1968Determinatin.
+
+
+
+
+
+
+@NOTES Lg/31
+
+See Von Soden 1931 "Hymnisch-epische Dialekt" 175-208.
+
+@NOTES Lg/31
+
+See {B}Lg/VonSoden1939Adverbialisendung.
+
+@NOTES Lg/31
+
+See Gelb 1952 *Morphology* 38-52.
+
+@NOTES Lg/31
+
+See Casstellino 1957 *Pronouns*.
+
+@NOTES Lg/31
+
+See {B}Lg/Jacobsen1960IttallakNiati.
+
+@NOTES Lg/31
+
+See {B}Lg/Jacobsen1963AblativeAccusative.
+
+@NOTES Lg/31
+
+See Gelb 1969 *Sequential* 171-86.
+
+
+
+
+
+@NOTES Lg/33
+
+See Stamm 1939 *Namengeburg*.
+
+@NOTES Lg/33
+
+See {B}Lg/Edzard1962NigalGamil.
+
+@NOTES Lg/33
+
+See Reiner 1966 *Analysis* 103-4.
+
+
+
+
+
+@NOTES Lg/35
+
+See Goetze 1946 "Syllables".
+
+@NOTES Lg/35
+
+See {B}Lg/Goetze1947ShortA.
+
+@NOTES Lg/35
+
+See {B}Lg/Jacobsen1960IttallakNiati.
+
+@NOTES Lg/35
+
+See Reiner 1966 *Analysis* 105-116.
+
+
+
+
+
+@NOTES Lg/39
+
+See {B}Lg/Botterweck1952Triliterismus.
+
+@NOTES Lg/39
+
+See Von Soden 1952 *Grundriss* Par. 50, 100, 104.
+
+@NOTES Lg/39
+
+See Reiner 1966 *Analysis* pp. 82-83.
+
+
+
+
+
+@NOTES Lg/39.1
+
+See {B}Lg/Hurwitz1913RootDeterminatives.
+
+@NOTES Lg/39.1
+
+See {B}Lg/Blake1942StudiesII.
+
+@NOTES Lg/39.1
+
+See {B}Lg/Moscati1947Biconsonantismo.
+
+@NOTES Lg/39.1
+
+See {B}Lg/Botterweck1952Triliterismus.
+
+@NOTES Lg/39.1
+
+See {B}Lg/Fleisch1961Traite.
+
+@NOTES Lg/39.1
+
+See {B}Lg/VonSoden1952UnregelmassigeVerben.
+
+@NOTES Lg/39.1
+
+See Von Soden 1952 *Grundriss* Par. 50, 100, 104.
+
+@NOTES Lg/39.1
+
+See Reiner 1966 *Analysis* pp. 82-83.
+
+
+
+
+
+@NOTES Lg/42
+
+See {B}Lg/Kienast1963Zweiradikalige.
+
+
+
+
+
+@NOTES Lg/43
+
+See Von Soden 1932 "Hymnisch-episch Dialekt" 155-59.
+
+@NOTES Lg/43
+
+See Kurylowicz 1961 *Apophonie* 59 on root vowel i/a.
+
+@NOTES Lg/43
+
+See {B}Lg/Kienast1963Zweiradikalige.
+
+
+
+
+
+@NOTES Lg/44
+
+See {B}Lg/Landsberger1926Prinzipienfragen.
+
+@NOTES Lg/44
+
+See {B}Lg/Kienast1963Zweiradikalige.
+
+@NOTES Lg/44
+
+See {B}Lg/Blake1942StudiesII.
+
+@NOTES Lg/44
+
+See {B}Lg/Knudsen1965Susa.
+
+
+
+
+@NOTES Lg/44.1
+
+See Sarauw 1939 *Akzent* 41, 42, 44.
+
+@NOTES Lg/44.1
+
+See {B}Lg/Fleisch1947-43Systeme.
+
+
+
+
+
+@NOTES Lg/45
+
+See {B}Lg/Kienast1963Zweiradikalige.
+
+
+
+
+
+@NOTES Lg/48
+
+See {B}Lg/VonSoden1952UnregelmassigeVerben.
+
+
+
+
+
+@NOTES Lg/57
+
+See Ungad 1903-4 "Syntax".
+
+@NOTES Lg/57
+
+See Von Soden 1952 *Grundriss*.
+
+
+
+
+
+@NOTES Lg/65
+
+See Kurylowicz 1961 *Apophonie* passim.
+
+@NOTES Lg/65
+
+See Rowton1962Permansive (1).
+
+@NOTES Lg/65
+
+See Aro 1964 *Vokalisierung* 9,180 and passim.
+
+@NOTES Lg/65
+
+See {B}Lg/Kienast1967Vokalklassen.
+
+
+
+
+
+@NOTES Lg/65.1
+
+See JJ. Finkelstein "Amnisadunga's Edict and the babylonian 'Law Codes'," *JCS* 15 (1961) 96.
+
+
+
+
+
+@NOTES Lg/66
+
+See Von Soden 1932 "Hymnisch-epische" 117,132,133-146.
+
+@NOTES Lg/66
+
+See {B}Lg/VonSoden1939Adverbialisendung.
+
+@NOTES Lg/66
+
+See Von Soden 1952 *Grundriss* §§ 113-15; 118-20.
+
+@NOTES Lg/66
+
+See {B}Lg/Jacobsen1960IttallakNiati.
+
+@NOTES Lg/66
+
+See Aro 1961 *Infinitivkonstruktionen*
+
+@NOTES Lg/66
+
+See {B}Lg/VonSoden1961Akkusativ.
+
+@NOTES Lg/66
+
+See {B}Lg/Jacobsen1963AblativeAccusative.
+
+@NOTES Lg/66
+
+See Aro 1963 "Pr&#228;positionale Verbindungen".
+
+
+
+
+
+@NOTES Lg/70
+
+See {B}Lg/Landsberger1926Prinzipienfragen.
+
+@NOTES Lg/70
+
+See Aro 1964 *Vokalisierung*.
+
+
+
+
+
+
+@NOTES Lg/72
+
+See Von Soden 1952 *Grundriss* §§88-94.
+
+@NOTES Lg/72
+
+See Catellino 1962 *System* 121-31.
+
+@NOTES Lg/72
+
+See {B}Lg/Edzard1965Stemme.
+
+
+
+
+
+@NOTES Lg/72.1
+
+See {B}Lg/Speiser1936Formatives.
+
+@NOTES Lg/72.1
+
+See {B}Lg/Goetze1942Intensive.
+
+@NOTES Lg/72.1
+
+See W.Von Soden, "Bin Zwiegespr&#228;ch Hammurabis mit einer Frau," *ZA* 49 (1950) 177f.
+
+@NOTES Lg/72.1
+
+See {B}Lg/Speiser1952Elative.
+
+@NOTES Lg/72.1
+
+See Casstellino 1964 "Causativo".
+
+
+
+
+
+@NOTES Lg/72.2
+
+See Oppenheim 1935 "t-Infixes".
+
+@NOTES Lg/72.2
+
+See {B}Lg/Goetze1936tForm.
+
+@NOTES Lg/72.2
+
+See {B}Lg/Speiser1955Hithpa'al.
+
+@NOTES Lg/72.2
+
+See {B}Lg/Rowton1962Permansive (1).
+
+
+
+
+
+@NOTES Lg/72.3
+
+See {B}Lg/Speiser1955Hithpa'al.
+
+@NOTES Lg/72.3
+
+See {B}Lg/Jacobsen1960IttallakNiati.
+
+
+
+
+
+@NOTES Lg/72.4
+
+See {B}Lg/Poebel1939Grammar.
+
+@NOTES Lg/72.4
+
+See {B}Lg/Rowton1962Permansive (1).
+
+
+
+
+
+@NOTES Lg/75
+
+See {B}Lg/Fleisch1959AspectLexical.
+
+
+
+
+
+@NOTES Lg/76
+
+See Meek 1950 "Explicative Pronoum".
+
+@NOTES Lg/76
+
+See {B}Lg/VonSoden1960StatusRectus-Formen.
+
+@NOTES Lg/76
+
+See Aro 1961 *Infinitivkonstruktionen*.
+
+@NOTES Lg/76
+
+See Aro 1963 "Pr&#228;positionale Verbindungen".
+
+@NOTES Lg/76
+
+See Reiner 1966 *Analysis* 125-127.
+
+@NOTES Lg/76
+
+See {B}Lg/Pennacchietti1968Determinatin.
+
+
+
+
+
+@NOTES Lg/77
+
+See {B}Lg/VonSoden1960StatusRectus-Formen.
+
+@NOTES Lg/77
+
+See Reiner 1966 *Analysis* 125-127.
+
+@NOTES Lg/77
+
+See {B}Lg/Kienast1967Vokalklassen.
+
+
+
+
+
+@NOTES Lg/81
+
+See {B}Lg/Ravn1941Relative.
+
+@NOTES Lg/81
+
+See {B}Lg/Reiner1951PropositionRelative.
+
+@NOTES Lg/81
+
+See {B}Lg/Fleisch1966PhraseRelative.
+
+
+
+
+
+
+@NOTES Lg/82
+
+See {B}Lg/Landsberger1924Ventiv.
+
+@NOTES Lg/82
+
+See {B}Lg/Barton1927Anticipatory.
+
+@NOTES Lg/82
+
+See {B}Lg/Poebel1932Appositionell.
+
+@NOTES Lg/82
+
+See {B}Lg/Blake1942StudiesII.
+
+@NOTES Lg/82
+
+See {B}Lg/Greenberg1960Agreement.
+
+@NOTES Lg/82
+
+See P. Garelli, *Les Assyriens en Cappedoce,* Paris 1963, p. 171 f.
+
+@NOTES Lg/82
+
+See Andersen, "Moabite Syntax" *Or.* NS 35 (1966) 112 f.
+
+@NOTES Lg/82
+
+See {B}Lg/Pennacchietti1968Determinatin.
+
+
+
+
+
+
+@NOTES Lg/83
+
+See Gelb 1969 *Sequential*.
+
+
+
+
+
+
+@NOTES Lg/84
+
+See {B}Lg/Speiser1936Formatives.
+
+@NOTES Lg/84
+
+See {B}Lg/Meek1946Asyndeton.
+
+@NOTES Lg/84
+
+See {B}Lg/Fleisch1956Esquissa.
+
+@NOTES Lg/84
+
+See {B}Lg/Rowton1962Permansive (1).
+
+@NOTES Lg/84
+
+See Batterson 1970 *Parataxis*.
+
+
+
+
+
+
+@NOTES Lg/85
+
+See {B}Lg/Speiser1947Summa.
+
+@NOTES Lg/85
+
+See Halder 1950 (JCS 4) 63 ff.
+
+@NOTES Lg/85
+
+See Halder 1963-64 "System" 25 f.
+
+@NOTES Lg/85
+
+See {B}Lg/VonSoden1965Perfekt.
